9c9a68e3e691f91006fc366a71a0740ac405f05c
Vadym Rusin <vadymrusin@Vadyms-MacBook-Pro.local>
2020-09-21 20:05:28 +0200

    DB dev/prod connection configuration

--- README.md
+++ README.md
@@ -1,3 +1,37 @@
-# Strapi application
+# RockUX
 
-A quick description of your strapi application
+## Development server
+
+- [rockux-platform-dev.herokuapp.com](https://rockux-platform-dev.herokuapp.com/) – API
+- [rockux-platform-dev.herokuapp.com/admin](https://rockux-platform-dev.herokuapp.com/admin) – Admin
+
+## Production server
+
+- [rockux-platform.herokuapp.com](https://rockux-platform.herokuapp.com/) – API
+- [rockux-platform.herokuapp.com/admin](https://rockux-platform.herokuapp.com/admin) – Admin
+
+---
+
+### Production connection `MongoDB`
+
+Create `.env` file, paste code bellow and update variables values
+
+```js
+DATABASE_HOST='cluster.foo.mongodb.net'
+DATABASE_SRV=true
+DATABASE_PORT=27017
+DATABASE_NAME='db_name'
+DATABASE_USERNAME='username'
+DATABASE_PASSWORD='password'
+```
+
+### Development connection `SQLite`
+
+Create `.env` file, paste code bellow and update variables values
+
+```js
+HOST=0.0.0.0
+PORT=1337
+
+MODE='development'
+```
--- config/database.js
+++ config/database.js
@@ -1,20 +1,42 @@
+const devConnection = function (env) {
+	return {
+		default: {
+			connector: 'bookshelf',
+			settings: {
+				client: 'sqlite',
+				filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+			},
+			options: {
+				useNullAsDefault: true,
+			},
+		},
+	}
+}
+
+const prodConnection = function (env) {
+	return {
+		default: {
+			connector: 'mongoose',
+			settings: {
+				host: env('DATABASE_HOST'),
+				srv: env.bool('DATABASE_SRV'),
+				port: env.int('DATABASE_PORT'),
+				database: env('DATABASE_NAME'),
+				username: env('DATABASE_USERNAME'),
+				password: env('DATABASE_PASSWORD'),
+			},
+			options: {
+				authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+				ssl: env.bool('DATABASE_SSL', true),
+			},
+		},
+	}
+}
+
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'mongoose',
-      settings: {
-        host: env('DATABASE_HOST'),
-        srv: env.bool('DATABASE_SRV'),
-        port: env.int('DATABASE_PORT'),
-        database: env('DATABASE_NAME'),
-        username: env('DATABASE_USERNAME'),
-        password: env('DATABASE_PASSWORD'),
-      },
-      options: {
-        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-        ssl: env.bool('DATABASE_SSL', true),
-      },
-    },
-  },
-});
+	defaultConnection: 'default',
+	connections:
+		env('MODE') === 'development'
+			? devConnection(env)
+			: prodConnection(env),
+})
