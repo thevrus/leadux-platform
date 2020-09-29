dee8a2daeca8fa880e2836e7e356c6db320a920e
thevrus <vadimrusin94@gmail.com>
2020-09-29 12:32:59 +0200

    Liqpay payment support added

--- extensions/users-permissions/controllers/UsersPermissions.js
+++ extensions/users-permissions/controllers/UsersPermissions.js
@@ -1,36 +1,40 @@
 'use strict'
 const { sanitizeEntity } = require('strapi-utils')
 
-/**
- * UsersPermissions.js controller
- *
- * @description: A set of functions called "actions" of the `users-permissions` plugin.
- */
+const liqpay = strapi.config.liqpay
 
 module.exports = {
-	/**
-	 * Default action.
-	 *
-	 * @return {Object}
-	 */
 	async setStudent(ctx) {
-		const { user } = ctx.state
-
-		if (!user) {
-			return ctx.badRequest(null, [
-				{ messages: [{ id: 'No authorization header was found' }] },
-			])
-		}
-
-		if (!ctx.request.body) {
+		if (Object.keys(ctx.request.body).length === 0) {
 			return ctx.badRequest(null, [
 				{ messages: [{ id: 'Request body is empty' }] },
 			])
 		}
 
-		const { order_id, transaction_id, paytype } = ctx.request.body.data
+		const POST_DATA = ctx.request.body.data
+		const POST_SIGNATURE = ctx.request.body.signature
 
-		if (!order_id && !transaction_id && !paytype) {
+		if (liqpay.signature(POST_DATA) !== POST_SIGNATURE) {
+			return ctx.badRequest(null, [
+				{ messages: [{ id: 'Signature is invalid' }] },
+			])
+		}
+
+		const {
+			status,
+			currency,
+			amount,
+			description,
+			sender_card_country,
+			order_id,
+			payment_id,
+			liqpay_order_id,
+			transaction_id,
+		} = JSON.parse(liqpay.decode_base64(ctx.request.body.data))
+
+		console.log(JSON.parse(liqpay.decode_base64(ctx.request.body.data)))
+
+		if (status !== 'success') {
 			return ctx.badRequest(null, [
 				{ messages: [{ id: 'Something went wrong' }] },
 			])
@@ -40,34 +44,66 @@ module.exports = {
 			.query('role', 'users-permissions')
 			.findOne({ type: 'student' }, [])
 
-		if (role && user) {
-			strapi.query('user', 'users-permissions').update(
-				{ id: user.id },
-				{
-					role,
-					orderID: order_id.toString(),
-					transactionID: transaction_id.toString(),
-					paymentService: paytype.toString(),
-				}
-			)
-		}
+		strapi.query('user', 'users-permissions').update(
+			{ id: ctx.request.query.id },
+			{
+				role,
+				currency,
+				amount,
+				description,
+				sender_card_country,
+				order_id,
+				payment_id,
+				liqpay_order_id,
+				transaction_id,
+			}
+		)
 
-		const sanitizeUser = user => {
-			sanitizeEntity(user, {
-				model: strapi.query('user', 'users-permissions').model,
-			})
-		}
+		await strapi.plugins['users-permissions'].services.user.fetch({
+			id: ctx.request.query.id,
+		})
 
-		let data = await strapi.plugins[
-			'users-permissions'
-		].services.user.fetch({ id: user.id })
-
-		if (data) data = sanitizeUser(data)
-
-		// @TODO
+		// TODO
 		// * Send confirmational email
+		ctx.redirect('http://localhost:8080/watch')
+	},
 
-		// Send 200 `ok`
-		ctx.body = data
+	async getInvoice(ctx) {
+		const { user } = ctx.state
+
+		if (!user) {
+			return ctx.badRequest(null, [
+				{ messages: [{ id: 'No authorization header was found' }] },
+			])
+		}
+
+		// TODO
+		// * Pass environment variable
+		const result_url = `${liqpay.host_url}/users-permissions/setstudent?id=${user.id}`
+
+		// TODO
+		// * Prepare individual invoices
+		// * Check if user has other plans
+		// const { status, country, countryCode, currency } = ctx.request.body
+		// console.log(ctx.request.body)
+
+		const payment = {
+			action: 'pay',
+			amount: '1',
+			currency: 'UAH',
+			public_key: liqpay.public_key,
+			description: 'Курс Figma',
+			result_url,
+			version: '3',
+		}
+
+		const { data, signature } = liqpay.data_signature(payment)
+
+		ctx.body = {
+			currency: payment.currency,
+			amount: payment.amount,
+			data,
+			signature,
+		}
 	},
 }
