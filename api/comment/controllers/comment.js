aa38abfe6cbd56dbd1c353cdc9b6da3288f7a46e
thevrus <vadimrusin94@gmail.com>
2020-10-06 12:25:25 +0200

    Comments content type fix

--- api/comment/controllers/comment.js
+++ api/comment/controllers/comment.js
@@ -1,30 +1,8 @@
 'use strict'
-const { sanitizeEntity, parseMultipartData } = require('strapi-utils')
 
-module.exports = {
-	async comment(ctx) {
-		console.log('create fired')
-		const user = ctx.state.user
+/**
+ * Read the documentation (https://strapi.io/documentation/v3.x/concepts/services.html#core-services)
+ * to customize this service
+ */
 
-		if (!user) {
-			return ctx.badRequest(null, [
-				{ messages: [{ id: 'You have to sign in first to comment.' }] },
-			])
-		}
-
-		let entity
-
-		// TODO
-		// * Support images/files
-		if (ctx.is('multipart')) {
-			const { data, files } = parseMultipartData(ctx)
-			entity = await strapi.services.comment.create(data, { files })
-		} else {
-			ctx.request.body.user = user
-			ctx.request.body.lesson = ctx.params.id
-			entity = await strapi.services.comment.create(ctx.request.body)
-		}
-
-		return sanitizeEntity(entity, { model: strapi.models.comment })
-	},
-}
+module.exports = {}
--- api/lesson/controllers/lesson.js
+++ api/lesson/controllers/lesson.js
@@ -1,5 +1,5 @@
 'use strict'
-
+const { parseMultipartData, sanitizeEntity } = require('strapi-utils')
 const { sanitizeLesson } = require('../../../helpers/helpers')
 
 module.exports = {
@@ -17,5 +17,29 @@ module.exports = {
 		}
 
 		return entities.map(entity => sanitizeLesson(entity, ctx))
+	},
+	async comment(ctx) {
+		const user = ctx.state.user
+
+		if (!user) {
+			return ctx.badRequest(null, [
+				{ messages: [{ id: 'You have sign in first to comment.' }] },
+			])
+		}
+
+		let entity
+
+		// TODO
+		// * Support images/files
+		if (ctx.is('multipart')) {
+			const { data, files } = parseMultipartData(ctx)
+			entity = await strapi.services.comment.create(data, { files })
+		} else {
+			ctx.request.body.user = user
+			ctx.request.body.lesson = ctx.params.id
+			entity = await strapi.services.comment.create(ctx.request.body)
+		}
+
+		return sanitizeEntity(entity, { model: strapi.models.comment })
 	},
 }
